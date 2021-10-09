690e5ec779ba637c0a8ed8531768c4fdd5b57ba0
Akhila PR <akhilapr1923@gmail.com>
2021-10-09 20:03:46 +0530

    init

---
+++ payment.php
@@ -0,0 +1,22 @@
+<?php
+//get data from form  
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email= $_POST['email'];
+$phnno = $_POST['phnno'];
+$message= $_POST['msg'];
+$to = "office@scorelinesports.in";
+$subject = "Mail From website";
+$txt ="First Name = ". $fname . "\r\nLast Name = " . $lname . "\r\nEmail = " . $email ."\r\nPhone Number = " . $phnno . "\r\nMessage =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+
+//redirect
+header("Location:qrcode.html");
+
+
+
+?>
